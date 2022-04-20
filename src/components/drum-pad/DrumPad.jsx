78ceb69837dbbf57fd077a720d17d4ccb4e1bf5d
Ghalia AlHasan <ghaliaalhasan12@gmail.com>
2022-04-20 14:21:55 +0300

    trigger audio upon pressing a key associated with a drum pad

--- src/components/drum-pad/DrumPad.jsx
+++ src/components/drum-pad/DrumPad.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Audio from '../audio/Audio'
 
 function DrumPad({ el }) {
@@ -6,6 +6,15 @@ function DrumPad({ el }) {
         const audioTag = document.getElementById(el.key)
         audioTag.play();
     }
+
+
+    useEffect(() => {
+        window.addEventListener('keypress', (e) => {
+            const audioTag = document.getElementById(e.key.toUpperCase())
+            audioTag.play()
+        })
+
+    }, [])
 
     return (
         <button onClick={playAudio} id={el.key + '-Key'} className='drum-pad'>{el.key}
